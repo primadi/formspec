aff14f32a6a24c2e551c841f66449dfecc06e4fb
primadi <primadisetiawan@gmail.com>
2026-09-11 04:01:00 +0000

    chore: bump contoh versi installer v0.0.5 → v0.0.6 (docs + site)

--- site/public/install.sh
+++ site/public/install.sh
@@ -6,8 +6,8 @@
 #
 # Opsi:
 #   sh install.sh                       # install versi terbaru (stable)
-#   sh install.sh --version v0.0.5     # install versi tertentu
-#   FORMSPEC_VERSION=v0.0.5 sh install.sh
+#   sh install.sh --version v0.0.6     # install versi tertentu
+#   FORMSPEC_VERSION=v0.0.6 sh install.sh
 #
 # Perilaku:
 #   - Tanpa sudo: binary dipasang di ~/.local/bin/formspec (user-local).
